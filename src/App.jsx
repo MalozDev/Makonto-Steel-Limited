53099e718c49b31c180e0732e37aedd74ba262d6
Stephan Malobeka <malozdev@gmail.com>
2025-06-13 13:02:43 +0200

    Initial commit -Project Structure set  Firebase and React- Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Hello World 👋</h1>
+      <p>Welcome to Makonto Steel Limited</p>
+    </div>
+  );
+}
+
+export default App;
